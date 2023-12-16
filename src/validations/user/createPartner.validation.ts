1df92d8c9f8ac4a9bc7426b00a868a557c3f8138
codewithemmy <codewithemmy@gmail.com>
2023-12-16 14:33:27 +0100

    update

--- src/validations/user/createPartner.validation.ts
+++ src/validations/user/createPartner.validation.ts
@@ -9,20 +9,11 @@ const createUserValidation = {
     errorMessage: "phone cannot be empty",
     trim: true,
   },
-  language: {
-    notEmpty: true,
-    errorMessage: "language cannot be empty",
-    trim: true,
-  },
   email: {
     notEmpty: true,
     isEmail: true,
     errorMessage: "Email cannot be empty",
     trim: true,
-  },
-  dataOfBirth: {
-    notEmpty: true,
-    errorMessage: "dataOfBirth cannot be empty",
   },
 }
 
